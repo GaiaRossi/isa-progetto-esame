5c784f44ac8f4d9128158d6197f0ea4f96724c33
Gaia Rossi <gaia.rossi@edu.unife.it>
2024-01-31 17:51:20 +0100

    creato il test per il model del progetto

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid project' do
+    project = Project.new(name: 'progetto', description: 'descrizione progetto')
+    assert project.valid?
+  end
+
+  test 'invalid without name' do
+    project = Project.new(description: 'descrizione progetto')
+    refute project.valid?, 'project is valid without name'
+  end
 end
