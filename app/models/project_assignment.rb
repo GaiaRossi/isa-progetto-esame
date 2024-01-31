a5687efb6a2b95a0b4b8c2e49310b636be8f932e
Gaia Rossi <gaia.rossi@edu.unife.it>
2024-01-31 18:38:15 +0100

    scritti i test per il model di project assignment e corretto il model di project assignment

--- app/models/project_assignment.rb
+++ app/models/project_assignment.rb
@@ -1,4 +1,7 @@
 class ProjectAssignment < ApplicationRecord
     belongs_to :user
     belongs_to :project
+
+    # blocca la coppia user_id e project_id
+    validates_uniqueness_of  :user_id, scope: :project_id 
 end
--- test/models/project_assignment_test.rb
+++ test/models/project_assignment_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class ProjectAssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid project assignment' do
+    user = User.create(username: 'utente1', is_admin: false, password: 'test_password_utente1')
+    project = Project.create(name: 'progetto', description: 'descrizione progetto')
+    project_assignment = ProjectAssignment.new(project_id: project.id, user_id: user.id)
+    assert project_assignment.valid?
+  end
+
+  test 'valid project assignment same project' do
+    user = User.create(username: 'utente1', is_admin: false, password: 'test_password_utente1')
+    project = Project.create(name: 'progetto', description: 'descrizione progetto')
+    project1 = Project.create(name: 'progetto1', description: 'descrizione progetto1')
+    project_assignment = ProjectAssignment.create(project_id: project.id, user_id: user.id)
+    project_assignment1 = ProjectAssignment.new(project_id: project1.id, user_id: user.id)
+    assert project_assignment1.valid?
+  end
+
+  test 'invalid with incorrect user_id' do
+    project = Project.create(name: 'progetto', description: 'descrizione progetto')
+    project_assignment = ProjectAssignment.new(project_id: project.id, user_id: -1)
+    refute project_assignment.valid?, 'project assignment created with incorrect user_id'
+  end
+
+  test 'invalid with incorrect project_id' do
+    user = User.create(username: 'utente1', is_admin: false, password: 'test_password_utente1')
+    project_assignment = ProjectAssignment.new(project_id: -1, user_id: user.id)
+    refute project_assignment.valid?, 'project assignment created with incorrect project_id'
+  end
+
+  test 'invalid if assignment already made' do
+    user = User.create(username: 'utente1', is_admin: false, password: 'test_password_utente1')
+    project = Project.create(name: 'progetto', description: 'descrizione progetto')
+    project_assignment = ProjectAssignment.create(project_id: project.id, user_id: user.id)
+    project_assignment_dup = ProjectAssignment.new(project_id: project.id, user_id: user.id)
+    refute project_assignment_dup.valid?, 'project assignment with assignment already made'
+  end
 end
