130d09efa7ffd5e168980dd94122f8c74c4acce1
Gaia Rossi <gaia.rossi@edu.unife.it>
2024-02-05 16:23:30 +0100

    on delete cascade da project alle dipendenze e da user alle dipendenze

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
-    has_many :tasks
-    has_many :project_assignment
+    has_many :tasks, dependent: :delete_all
+    has_many :project_assignment, dependent: :delete_all
     has_many :user, :through => :project_assignment
     validates :name, presence: true, uniqueness: true
     scope :assigned_to, -> (user) {
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password :password
-    has_many :project_assignment
+    has_many :project_assignment, dependent: :delete_all
     has_many :projects, :through => :project_assignment
     validates :username, :password, presence: true
     validates :username, uniqueness: true
