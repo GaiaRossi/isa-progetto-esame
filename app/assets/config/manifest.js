6e932070d39c0b686a9e8f2727565d250b9c5947
Gaia Rossi <gaia.rossi@edu.unife.it>
2024-02-09 20:00:11 +0100

    aggiunti i file per uso bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link application.scss
+//= link application.bootstrap.scss
+//= link application.css
+//= link bootstrap.min.js
