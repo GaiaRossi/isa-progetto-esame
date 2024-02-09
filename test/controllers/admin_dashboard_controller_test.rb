b7b1835529a4b55870d9155e792cb6562295a98b
Gaia Rossi <gaia.rossi@edu.unife.it>
2024-02-09 19:43:27 +0100

    creati i test per il controller della dashboard per gli admin

--- test/controllers/admin_dashboard_controller_test.rb
+++ test/controllers/admin_dashboard_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class AdminDashboardControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    log_in_as_admin
+  end
+
   test "should get index" do
     get admin_dashboard_index_url
     assert_response :success
